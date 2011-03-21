162433bec8358fb4ee5deae063357714ca2ef32f
Felipe Ribeiro <felipernb@gmail.com>
2011-03-21 12:24:50 +0100

    Adding implementation for MinHeap

--- data_structures/heap.php
+++ data_structures/heap.php
@@ -1,25 +1,28 @@
 <?php
 /** 
- * Implementation of the (Max)Heap data structure
+ * Implementation of the Heap data structure
  *
  * (PHP offers it natively as SPLMaxHeap/SPLMinHeap)
  *
  * @author Felipe Ribeiro <felipernb@gmail.com>
  */
 
-class Heap {
-	private $elements = array();
-	private $n = 0;
+abstract class Heap {
+	protected $elements = array();
+	protected $n = 0;
 	
 	/**
 	 * Inserts a value to the heap, keeping the heap property
 	 * 
 	 * @author Felipe Ribeiro <felipernb@gmail.com>
 	 */
-	public function insert($element) {
-		$this->elements[++$this->n] = $element;
-		for ($i = $this->n; $i > 1 && $this->elements[$i >> 1] < $this->elements[$i]; $i = $i >> 1)
-			$this->swap($i >> 1,$i);
+	public abstract function insert($element);
+
+	/**
+	 * @author Felipe Ribeiro <felipernb@gmail.com>
+	 */
+	public function isEmpty() {
+		return $this->n==0;
 	}
 
 	/**
@@ -31,16 +34,44 @@ class Heap {
 		$element = $this->elements[1];
 		$this->elements[1] = array_pop($this->elements);
 		$this->n--;
-		$this->heapify();
+		$this->siftDown();
 		return $element;
 	}
-
+	
 	/**
 	 * Rearranges the heap after an extraction to keep the heap property
 	 *
 	 * @author Felipe Ribeiro <felipernb@gmail.com>
 	 */
-	private function heapify() {
+	protected abstract function siftDown();
+
+	/**
+	 * Swap two elements on the elements array
+	 *
+	 * @author Felipe Ribeiro <felipernb@gmail.com>
+	 */
+	protected function swap($x,$y) {
+		$tmp = $this->elements[$x];
+		$this->elements[$x] = $this->elements[$y];
+		$this->elements[$y] = $tmp;
+	}
+}
+
+class MaxHeap extends Heap {
+	
+	/**
+	 * @see Heap::insert
+	 */
+	public function insert($element) {
+		$this->elements[++$this->n] = $element;
+		for ($i = $this->n; $i > 1 && $this->elements[$i >> 1] < $this->elements[$i]; $i = $i >> 1)
+			$this->swap($i >> 1,$i);
+	}
+
+	/**
+	 * @see Heap::insert
+	 */
+	protected function siftDown() {
 		for ($i = 1; ($c = $i * 2) <= $this->n; $i = $c) {
 			//Checks which of the bigger child to compare with the parent
 			if ($c+1 <= $this->n && $this->elements[$c+1] > $this->elements[$c])
@@ -51,15 +82,34 @@ class Heap {
 		}
 	}
 
+}
+
+
+class MinHeap extends Heap {
+	
 	/**
-	 * Swap two elements on the elements array
-	 *
-	 * @author Felipe Ribeiro <felipernb@gmail.com>
+	 * @see Heap::insert
 	 */
-	private function swap($x,$y) {
-		$tmp = $this->elements[$x];
-		$this->elements[$x] = $this->elements[$y];
-		$this->elements[$y] = $tmp;
+	public function insert($element) {
+		$this->elements[++$this->n] = $element;
+		for ($i = $this->n; $i > 1 && $this->elements[$i >> 1] > $this->elements[$i]; $i = $i >> 1)
+			$this->swap($i >> 1,$i);
+	}
+
+	/**
+	 * @see Heap::siftDown
+	 */
+	protected function siftDown() {
+		for ($i = 1; ($c = $i * 2) <= $this->n; $i = $c) {
+			//Checks which of the smaller child to compare with the parent
+			if ($c+1 <= $this->n && $this->elements[$c+1] < $this->elements[$c])
+				$c++;
+			if ($this->elements[$i] < $this->elements[$c])
+				break;
+			$this->swap($c, $i);
+		}
 	}
 
 }
+
+
