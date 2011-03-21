f9f1c825bc8ad6b2d3e0d1ae7cf2c03b3640dc8a
Felipe Ribeiro <felipernb@gmail.com>
2011-03-21 12:25:06 +0100

    Adding HeapSort

---
+++ sorting/heap_sort.php
@@ -0,0 +1,20 @@
+<?php
+require '../data_structures/heap.php';
+
+/**
+ * Uses a MinHeap to sort an array
+ * O(n lgn) in time and O(n) in space
+ *
+ * @author Felipe Ribeiro <felipernb@gmail.com>
+ */
+function heap_sort(&$a) {
+	$heap = new MinHeap();
+	foreach($a as $x) {
+		$heap->insert($x);
+	}
+
+	$a = array();
+	while(!$heap->isEmpty()) {
+		$a[] = $heap->extract();
+	}
+}
