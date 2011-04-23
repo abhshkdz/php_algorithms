e0cb7e0acd882251ea569cf88e30ac26bcac3fdd
Felipe Ribeiro <felipernb@gmail.com>
2011-04-23 21:56:58 +0200

    Adding topological sorting

--- graphs/topological_sorting.php
+++ graphs/topological_sorting.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * In computer science, a topological sort or topological ordering of a directed
+ * graph is a linear ordering of its vertices such that, for every edge uv, u 
+ * comes before v in the ordering. For instance, the vertices of the graph may
+ * represent tasks to be performed, and the edges may represent constraints that
+ * one task must be performed before another; in this application, a topological
+ * ordering is just a valid sequence for the tasks. A topological ordering is
+ * possible if and only if the graph has no directed cycles, that is, if it is
+ * a directed acyclic graph (DAG). Any DAG has at least one topological ordering,
+ * and algorithms are known for constructing a topological ordering of any DAG
+ * in linear time
+ *
+ * @author Felipe Ribeiro <felipernb@gmail.com>
+ */
 define('UNVISITED', 0);
 define('VISITED', 1);
 
@@ -40,5 +54,3 @@ $graph = array(
 	);
 $stack = topological_sorting($graph);
 while(!$stack->isEmpty()) echo($stack->pop(). "\n");
-
-
